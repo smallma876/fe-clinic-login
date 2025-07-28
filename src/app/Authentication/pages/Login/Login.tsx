aab9ee3e00a4d2bbdb32bbd3f78be865427fad0a
Sergio Mallma Chavez <sergio.mallma876@gmail.com>
2025-07-27 19:23:40 -0500

    chore: update components library

--- src/app/Authentication/pages/Login/Login.tsx
+++ src/app/Authentication/pages/Login/Login.tsx
@@ -7,7 +7,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import ClinicError from '@/shared/clinic-error.ts/ClinicError';
 import { useAppDispatch } from '@/store/app-context';
 import { handlerError } from '@/shared/handler-error/handler-error';
-import { ButtonPrimary, Checkbox, PasswordField, Selector, TextField, Card, ButtonLink } from '@sergio.mallma/fe-clinic-components';
+import { ButtonPrimary, Checkbox, PasswordField, Selector, TextField, Card, ButtonLink, TwoColumnLayout } from '@sergio.mallma/fe-clinic-components';
 
 const Login: FC = () => {
   const dispatchApp = useAppDispatch();
@@ -43,42 +43,46 @@ const Login: FC = () => {
   };
 
   return (
-    <form className="flex flex-col w-full">
-      <Card>
-        <h1>Login</h1>
-        <p>Ingresa tus datos para ingresar</p>
-        <Selector
-          id='documentType'
-          label='Tipo de documento'
-        >
-          <option value="dni">DNI</option>
-          <option value="passport">PASAPORTE</option>
-        </Selector>
-        <TextField
-          label='Documento'
-          id="document"
-          placeholder="document"
-          error={errors[LoginFields.Document] ? { message: errors[LoginFields.Document]?.message ?? "" } : undefined}
-          {...register('document', { required: 'Document is required' })}
-        />
-        <PasswordField
-          label='Contraseña'
-          placeholder="password"
-          id="password"
-          error={errors[LoginFields.Password] ? { message: errors[LoginFields.Password]?.message ?? "" } : undefined}
-          {...register('password', { required: 'Password is required' })}
-        />
-        <Checkbox
-          id="remember"
-          label='Recordar usuario'
-          value="remember"
-        />
-        <ButtonPrimary onClick={onLogin} isLoading={false} disabled={!isValid}>
-          Ingresar
-        </ButtonPrimary>
-        <ButtonLink onClick={onRegister}>Registrar</ButtonLink>
-      </Card>
-    </form>
+    <div className="flex flex-col w-full h-screen md:p-10 items-center">
+      <TwoColumnLayout
+        rightContent={<div>Hola Mundo</div>}
+      >
+        <Card>
+          <h1>Login</h1>
+          <p>Ingresa tus datos para ingresar</p>
+          <Selector
+            id='documentType'
+            label='Tipo de documento'
+          >
+            <option value="dni">DNI</option>
+            <option value="passport">PASAPORTE</option>
+          </Selector>
+          <TextField
+            label='Documento'
+            id="document"
+            placeholder="document"
+            error={errors[LoginFields.Document] ? { message: errors[LoginFields.Document]?.message ?? "" } : undefined}
+            {...register('document', { required: 'Document is required' })}
+          />
+          <PasswordField
+            label='Contraseña'
+            placeholder="password"
+            id="password"
+            error={errors[LoginFields.Password] ? { message: errors[LoginFields.Password]?.message ?? "" } : undefined}
+            {...register('password', { required: 'Password is required' })}
+          />
+          <Checkbox
+            id="remember"
+            label='Recordar usuario'
+            value="remember"
+          />
+          <ButtonPrimary onClick={onLogin} isLoading={false} disabled={!isValid}>
+            Ingresar
+          </ButtonPrimary>
+          <ButtonLink onClick={onRegister}>Registrar</ButtonLink>
+        </Card>
+      </TwoColumnLayout>
+    </div>
   );
 };
 
